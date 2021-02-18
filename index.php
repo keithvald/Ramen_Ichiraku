1e4b404c27bffb9325dd58a219a8dde5cf37cf86
Anissa Fabre <anissafabrepro@gmail.com>
2021-02-18 14:41:19 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device_width, initial-scale=1.0, user-scalable=no">
+	<title>Ramen Ichiraku</title>
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap/bootstrap-grid.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+
+</body>
+</html>
