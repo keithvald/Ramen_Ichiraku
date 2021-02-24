3b579776a0030e9f8efabc28c53b83b9a71d5862
Anissa Fabre <anissafabrepro@gmail.com>
2021-02-24 12:11:48 +0100

    termine

--- index.php
+++ index.php
@@ -10,14 +10,36 @@
 </head>
 
 <body>
-	<h1>Ramen Ichiraku</h1>  
+<!-- NAVIGATION-->
+	<nav class="navbar navbar-expand-lg">
+	  <a class="navbar-brand" href="#">Ramen Ichiraku</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    <ul class="navbar-nav ml-auto">
+	      <li class="nav-item mr-3 active">
+	        <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="#">Features</a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="#">Pricing</a>
+	      </li>
+	    </ul>
+	  </div>
+	</nav>
 
+
+
+<!-- ELEMENTS DU TABLEAU-->
 	<?php require 'pages/first.php' ;?>
 	<?php require 'pages/second.php' ;?>
 	<?php require 'pages/third.php' ;?>
 	<?php require 'pages/fourth.php' ;?>
-
-	<table class="container table table-striped table-dark">
+<!-- TABLEAU-->
+	<table class="container mt-5 table table-striped">
 	  <tbody>
 	    <tr>
 	      <th scope="row">1</th>
